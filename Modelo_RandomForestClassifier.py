8b83e45c53547e71a0f7f2366712c262d088ff80
DavidZuletaM <dazuleta@bancolombia.com.co>
2025-02-06 22:33:23 -0500

    Actualización Modelo_RandomForestClassifier.py
    Se actualizó el script, refinandolo y agregando la información obtenida por Feature Engineering de la fuente con los resultados de los modelos analíticos para cobranza.

--- Modelo_RandomForestClassifier.py
+++ Modelo_RandomForestClassifier.py
@@ -11,27 +11,14 @@ df_prob["key"] = df_prob["nit_enmascarado"].astype(str) + "_" + df_prob["num_obl
 #Feature Engineering
 
 #Base histórica de pagos del cliente
-df_hist_pagos_valor_cuota_mes=df_hist_pagos.groupby("key").agg({"valor_cuota_mes":"mean"}).reset_index()
-df_hist_pagos_valor_cuota_mes.columns=["key","promedio_valor_cuota_mes"]
-
-df_hist_pagos_pago_total=df_hist_pagos.groupby("key").agg({"pago_total":["sum","count","mean"]}).reset_index()
-df_hist_pagos_pago_total.columns=["key","pago_total","conteo_total_pagos","promedio_pago_mes"]
-
-df_hist_pagos_porc_pago=df_hist_pagos.groupby("key").agg({"porc_pago":"mean"}).reset_index()
-df_hist_pagos_porc_pago.columns=["key","promedio_porc_pago_cuota_mes"]
-
-df_hist_pagos_fecha_pago_min=df_hist_pagos.groupby("key").agg({"fecha_pago_minima":"min"}).reset_index()
-df_hist_pagos_fecha_pago_min.columns=["key","primer_fecha_pago"]
-
-df_hist_pagos_fecha_pago_max=df_hist_pagos.groupby("key").agg({"fecha_pago_maxima":"max"}).reset_index()
-df_hist_pagos_fecha_pago_max.columns=["key","ult_fecha_pago"]
-
-df_hist_pagos_producto=df_hist_pagos.groupby("key").agg({"producto": "first"}).reset_index()
-df_hist_pagos_producto.columns=["key","producto"]
+df_hist_pagos=df_hist_pagos.groupby("key").agg({"valor_cuota_mes":"mean","pago_total":["sum","count","mean"],"porc_pago":"mean","fecha_pago_minima":"min","fecha_pago_maxima":"max","producto":"first"}).reset_index()
+df_hist_pagos.columns=["key","promedio_valor_cuota_mes","pago_total","conteo_total_pagos","promedio_pago_mes","promedio_porc_pago_cuota_mes","primer_fecha_pago","ult_fecha_pago","producto"]
 
 df_hist_pagos_ajustes_banco=df_hist_pagos.pivot_table(index='key', columns='ajustes_banco', values= 'nit_enmascarado', aggfunc= 'count', fill_value=0).reset_index()
 df_hist_pagos_ajustes_banco.columns=["Key","tipo_ajuste_ajustes","tipo_ajuste_sin_ajustes","tipo_ajuste_rediferidos"]
 
 #Base resultados modelos analíticos para cobranza
 
+df_prob=df_prob.groupby("key").agg({"prob_propension":"mean","prob_alrt_temprana":"mean","prob_auto_cura":"mean","lote": lambda x: x.mode().iloc[0]}).reset_index()
+df_prob.columns=["key","prob_prom_propension_mes","prob_prom_alrt_temprana","prob_prom_auto_cura","lote_moda"]
 
